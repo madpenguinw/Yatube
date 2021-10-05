6b23d36bccf7deb840b9ed1705245e8baf724ee7
Mikhail Sokolov <lmikhailsokolovl@gmail.com>
2021-10-05 06:04:19 +0300

    Перед отправкой на ревью. 6 спринт

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -128,11 +128,7 @@ def follow_index(request):
 
 @login_required
 def profile_follow(request, username):
-    author = get_object_or_404(User, username=username)
-    if (
-        not Follow.objects.filter(author=author, user=request.user).exists()
-        or request.user != author
-    ):
+    if request.user.username != username:
         Follow.objects.get_or_create(
             user=request.user,
             author=get_object_or_404(User, username=username)
