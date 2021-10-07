f71a2dae63eeee5b64712249ffb234423ecb6b8e
Mikhail Sokolov <lmikhailsokolovl@gmail.com>
2021-10-07 17:53:02 +0300

    После третьего ревью. 6 спринт

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -126,12 +126,11 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if author:
-        if request.user.username != author:
-            Follow.objects.get_or_create(
-                user=request.user,
-                author=author
-            )
+    if request.user != author:
+        Follow.objects.get_or_create(
+            user=request.user,
+            author=author
+        )
     return redirect('posts:profile', username=username)
 
 
